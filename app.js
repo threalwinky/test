7aa67d8d813ce76bff1d3e67ae6343e9e2e4a793
Vũ <voquangvu09112006@gmail.com>
2024-07-30 01:41:25 +0700

    upload files

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express')
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', async (req, res) => {
+  let resp = req.query.message
+  // let resp = req.query.message
+  if (resp == undefined){
+    res.send('Hello')
+  }
+  else
+  res.send(resp + '123')
 });
 
 app.listen(8000, () => {
